8e6863901d3967d27b07fac110756ae82f45c471
Faiz Chaudhry <faiz.chaudhry@mail.utoronto.ca>
2019-03-11 14:21:29 -0400

    properly foldered the tests

---
+++ src/test/java/verse/engine/manualTesting/collision/CollisionTesting.java
@@ -0,0 +1,20 @@
+package verse.engine.manualTesting.collision;
+
+import javax.swing.SwingUtilities;
+
+import verse.engine.graphics.VFrame;
+
+public class CollisionTesting {
+	
+	public static void main(String args[]) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			public void run() {
+				new VFrame("Testing", VFrame.STANDARD, new CollisionTestingPanel(60, VFrame.STANDARD));
+			}
+		});
+		
+	}
+
+}
